a1d219579c00bb1ada2217a1085da393f3b2f1ba
Network Sec <85315993+Network-Sec@users.noreply.github.com>
2025-12-26 03:04:05 +0100

    Delete extDev directory

--- extDev/utils.ts
+++
@@ -1,6 +0,0 @@
-export const getServerUrl = (): string => {
-  // In development, this might differ. For now, assume relative path or env var.
-  // If you are running locally with a separate backend port:
-  // return 'http://localhost:3000'; 
-  return '';
-};
